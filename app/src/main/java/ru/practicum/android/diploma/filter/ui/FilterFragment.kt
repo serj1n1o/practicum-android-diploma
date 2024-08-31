281f5793f347509544f9039074c4e68ead21f3bb
GabovAleksander <avlagab@yandex.ru>
2024-08-31 18:38:43 +0300

    #4 Изменена структура, добавлен граф навигации и заглушки экранов.

---
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/FilterFragment.kt
@@ -0,0 +1,21 @@
+package ru.practicum.android.diploma.filter.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentFilterBinding
+
+class FilterFragment: Fragment(R.layout.fragment_filter) {
+    private var _binding: FragmentFilterBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentFilterBinding.bind(view)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -0,0 +1,43 @@
+package ru.practicum.android.diploma.root.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import ru.practicum.android.diploma.BuildConfig
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
+
+class RootActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityRootBinding.inflate(layoutInflater).also {setContentView(it.root)}
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.searchFragment, R.id.favoritesFragment, R.id.aboutFragment),
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
+        )
+
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+        binding.navigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+            }
+        }
+
+        // Пример использования access token для HeadHunter API
+        networkRequestExample(accessToken = BuildConfig.HH_ACCESS_TOKEN)
+
+        // Доступ к baseUrl HH так же через BuildConfig
+        BuildConfig.HH_BASE_URL
+    }
+
+    private fun networkRequestExample(accessToken: String) {
+        // ...
+    }
+
+}
