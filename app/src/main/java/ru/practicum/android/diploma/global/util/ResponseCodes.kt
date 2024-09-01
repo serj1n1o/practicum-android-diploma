364f0d4d006aeaf6bc71528930065b0f17c4de41
Sergey Bryukhanov <serjiniobryukhanov@gmail.com>
2024-09-01 18:16:21 +0300

    Изменил название класса с кодами ошибок

--- app/src/main/java/ru/practicum/android/diploma/global/util/ResponseCodes.kt
+++ app/src/main/java/ru/practicum/android/diploma/global/util/ResponseCodes.kt
@@ -1,6 +1,6 @@
 package ru.practicum.android.diploma.global.util
 
-object CodesResponses {
+object ResponseCodes {
     const val CODE_OK = 200
     const val CODE_NO_CONNECT = -1
     const val CODE_BAD_REQUEST = 400
