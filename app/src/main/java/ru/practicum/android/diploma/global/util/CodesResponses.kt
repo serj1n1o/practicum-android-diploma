1b9073e32d26256bbaf2b0bd77626a1dbb771628
Sergey Bryukhanov <serjiniobryukhanov@gmail.com>
2024-09-01 17:24:42 +0300

    Добавлены вспомогательные классы: для проверки интернета NetworkUtil, Debounce для отложенного запроса, CodesResponses - описал базовые коды ошибок для обработки, CustomFragment - наследник фрагмента с реализацией viewBinding и защиты от множественных нажатий.

---
+++ app/src/main/java/ru/practicum/android/diploma/global/util/CodesResponses.kt
@@ -0,0 +1,9 @@
+package ru.practicum.android.diploma.global.util
+
+object CodesResponses {
+    const val CODE_OK = 200
+    const val CODE_NO_CONNECT = -1
+    const val CODE_BAD_REQUEST = 400
+    const val CODE_NO_FOUND = 1
+    const val CODE_VACANCY_HAVE_NOT = 404 // вроде такой ответ должен придти если вакансия удалена и не доступна
+}
--- app/src/main/java/ru/practicum/android/diploma/vacancy/ui/VacancyFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/vacancy/ui/VacancyFragment.kt
@@ -1,21 +1,23 @@
 package ru.practicum.android.diploma.vacancy.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import androidx.fragment.app.Fragment
-import ru.practicum.android.diploma.R
+import android.view.ViewGroup
+import android.widget.TextView
 import ru.practicum.android.diploma.databinding.FragmentVacancyBinding
+import ru.practicum.android.diploma.global.util.CustomFragment
 
-class VacancyFragment : Fragment(R.layout.fragment_vacancy) {
-    private var _binding: FragmentVacancyBinding? = null
+class VacancyFragment : CustomFragment<FragmentVacancyBinding>() {
+
+    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentVacancyBinding {
+        return FragmentVacancyBinding.inflate(inflater, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentVacancyBinding.bind(view)
+        val example: TextView = binding.buildConfigReadExampleTextView // пример использования, binding доступен
+        clickDebounce() // так же сразу доступен
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
