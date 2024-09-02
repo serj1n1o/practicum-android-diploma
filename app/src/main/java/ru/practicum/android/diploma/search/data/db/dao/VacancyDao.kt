204a10a621129748359ac8edc5a7799db7ab3534
SirGeorga <fedins.pochta@gmail.com>
2024-09-03 00:08:36 +0700

    #21 готов каркас БД для экрана поиска вакансий

---
+++ app/src/main/java/ru/practicum/android/diploma/search/data/db/dao/VacancyDao.kt
@@ -0,0 +1,17 @@
+package ru.practicum.android.diploma.search.data.db.dao
+
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import ru.practicum.android.diploma.search.data.db.entity.VacancyEntity
+
+interface VacancyDao {
+    @Insert(entity = VacancyEntity::class, onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertVacancy(vacancyEntity:VacancyEntity)
+
+    @Query("SELECT * FROM vacancy_table")
+    suspend fun getAllVacancies(): List<VacancyEntity>
+
+    @Query("DELETE FROM vacancy_table WHERE id = :vacancyId")
+    suspend fun deleteVacancy(vacancyId: Int)
+}
---
+++ app/src/main/java/ru/practicum/android/diploma/search/data/db/entity/VacancyEntity.kt
@@ -0,0 +1,24 @@
+package ru.practicum.android.diploma.search.data.db.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "vacancy_table")
+data class VacancyEntity(
+    @PrimaryKey
+    val id: String,
+    val name: String?,
+    val area: String?,
+    val link: String?,
+    val address: String?,
+    val contactName: String?,
+    val contactPhone: String?,
+    val contactEmail: String?,
+    val employerName: String?,
+    val salary: String?,
+    val schedule: String?,
+    val skills: String?,
+    val experience: String?,
+    val description: String?,
+    val isFavorite: Boolean
+)
