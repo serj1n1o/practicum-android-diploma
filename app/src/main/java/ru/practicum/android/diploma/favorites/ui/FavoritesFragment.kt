7ead82e20645e90ae36951e6091eac3809535d58
SirGeorga <fedins.pochta@gmail.com>
2024-09-03 02:22:59 +0700

    #28 favourite vacancies layout done

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesFragment.kt
@@ -4,13 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.practicum.android.diploma.databinding.FragmentFilterBinding
+import ru.practicum.android.diploma.databinding.FragmentFavoritesBinding
 import ru.practicum.android.diploma.global.util.CustomFragment
 
-class FavoritesFragment : CustomFragment<FragmentFilterBinding>() {
+class FavoritesFragment : CustomFragment<FragmentFavoritesBinding>() {
 
-    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentFilterBinding {
-        return FragmentFilterBinding.inflate(inflater, container, false)
+    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentFavoritesBinding {
+        return FragmentFavoritesBinding.inflate(inflater, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
