c422c0ee97bee7acfd5aa37799fcdd165dfdd6b4
RinatGordey <140505799+RinatGordey@users.noreply.github.com>
2024-09-05 22:00:01 +0500

    Создал FavoritesFragment

--- app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/favorites/ui/FavoritesFragment.kt
@@ -4,17 +4,88 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.practicum.android.diploma.databinding.FragmentFilterBinding
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.FragmentFavoritesBinding
+import ru.practicum.android.diploma.favorites.domain.models.FavoriteState
+import ru.practicum.android.diploma.favorites.view_model.FavoriteVacancyFragmentViewModel
 import ru.practicum.android.diploma.global.util.CustomFragment
+import ru.practicum.android.diploma.search.domain.model.Vacancy
+import ru.practicum.android.diploma.search.ui.VacancyAdapter
 
-class FavoritesFragment : CustomFragment<FragmentFilterBinding>() {
+class FavoritesFragment : CustomFragment<FragmentFavoritesBinding>(), VacancyAdapter.VacancyClickListener {
 
-    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentFilterBinding {
-        return FragmentFilterBinding.inflate(inflater, container, false)
+    private val viewModel by viewModel<FavoriteVacancyFragmentViewModel>()
+    private var adapter: VacancyAdapter? = null
+    private var isClickAllowed = true
+
+    override fun createBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentFavoritesBinding {
+        return FragmentFavoritesBinding.inflate(inflater, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        adapter = VacancyAdapter(java.util.ArrayList(),this)
+//        binding.rv_favorites.layoutManager = LinearLayoutManager(requireContext(),
+//            LinearLayoutManager.VERTICAL, false)
+//        binding.rv_favorites.adapter = adapter
+
+        viewModel.observeState().observe(viewLifecycleOwner) { state ->
+            render(state)
+        }
+        viewModel.fillData()
     }
 
+    override fun onVacancyClick(vacancy: Vacancy) {
+        if (clickDebounce()) {
+            isClickAllowed = true
+
+ //           findNavController().navigate(
+//                R.id.action_favoriteVacancyFragment_to_vacancyDetailFragment,
+//                VacancyDetailFragment.createArgs(vacancy))
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        viewModel.fillData()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+        adapter = null
+    }
+
+    private fun render(state: FavoriteState) {
+        when (state) {
+            is FavoriteState.Content -> showContent(state.vacancies)
+            is FavoriteState.Empty -> showEmpty()
+        }
+    }
+
+    private fun showContent(vacancies: List<Vacancy>) {
+        binding.apply {
+//            rv_favorites.isVisible = true
+//            ll_favorites_empty.isVisible = false
+//            favorite_nothing_found.isVisible = false
+//            rv_favorites.adapter = adapter
+            adapter?.vacancyList = vacancies as ArrayList<Vacancy>
+            adapter?.notifyDataSetChanged()
+        }
+    }
+
+    private fun showEmpty() {
+        binding.apply {
+//            rv_favorites.isVisible = false
+//            ll_favorites_empty.isVisible = true
+        }
+    }
+
+    companion object {
+        fun newInstance() = FavoritesFragment()
+    }
 }
---
+++ app/src/main/java/ru/practicum/android/diploma/search/ui/VacancyAdapter.kt
@@ -0,0 +1,37 @@
+package ru.practicum.android.diploma.search.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.search.domain.model.Vacancy
+
+class VacancyAdapter(
+    var vacancyList: List<Vacancy>,
+    private var itemClickListener: VacancyClickListener,
+) : RecyclerView.Adapter<VacancyViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): VacancyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.vacancy_list_item, parent, false)
+        return VacancyViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: VacancyViewHolder, position: Int) {
+        val vacancy = vacancyList[position]
+        holder.bind(vacancy)
+
+        holder.itemView.setOnClickListener {
+            itemClickListener.onVacancyClick(vacancy)
+        }
+    }
+
+    override fun getItemCount(): Int{
+        return vacancyList.size
+    }
+
+    fun interface VacancyClickListener {
+        fun onVacancyClick(vacancy: Vacancy)
+    }
+}
