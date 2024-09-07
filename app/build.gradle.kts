5d164b1df352164bc62689279376ad83021e71ff
Sergey Bryukhanov <serjiniobryukhanov@gmail.com>
2024-09-07 17:53:47 +0300

    поле value в buildConfig для baseUrl вернул обратно, не компилируется в строку без экранирования, выдает ошибку. Добавил /idea в gitignore.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         buildConfigField(type = "String", name = "HH_ACCESS_TOKEN", value = "\"${developProperties.hhAccessToken}\"")
-        buildConfigField(type = "String", name = "HH_BASE_URL", value = "https://api.hh.ru/")
+        buildConfigField(type = "String", name = "HH_BASE_URL", value = "\"https://api.hh.ru\"")
     }
 
     buildTypes {
