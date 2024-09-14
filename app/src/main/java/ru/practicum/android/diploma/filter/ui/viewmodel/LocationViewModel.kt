9e98415dc14d841aefa224eb691f8bcd59939bfb
RinatGordey <140505799+RinatGordey@users.noreply.github.com>
2024-09-14 22:05:38 +0500

    исправляю ошибки detect2

--- app/src/main/java/ru/practicum/android/diploma/filter/ui/viewmodel/LocationViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/filter/ui/viewmodel/LocationViewModel.kt
@@ -1,47 +1,43 @@
 package ru.practicum.android.diploma.filter.ui.viewmodel
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
 
-class LocationViewModel() : ViewModel() {
+class LocationViewModel : ViewModel() {
+    /*
+        private val _countries = MutableLiveData<List<String>>() // Список стран
+        val countries: LiveData<List<String>> get() = _countries
 
-    private val _countries = MutableLiveData<List<String>>() // Список стран
-    val countries: LiveData<List<String>> get() = _countries
+        private val _regions = MutableLiveData<List<String>>() // Список регионов
+        val regions: LiveData<List<String>> get() = _regions
 
-    private val _regions = MutableLiveData<List<String>>() // Список регионов
-    val regions: LiveData<List<String>> get() = _regions
+        // Состояние для обработки ошибок
+        private val _errorState = MutableLiveData<String?>()
+        val errorState: LiveData<String?> get() = _errorState
 
-    // Состояние для обработки ошибок
-    private val _errorState = MutableLiveData<String?>()
-    val errorState: LiveData<String?> get() = _errorState
+        init {
+            loadCountries()
+        }
 
-    init {
-        loadCountries()
-    }
-
-    private fun loadCountries() {
-        viewModelScope.launch {
-            try {
-            } catch (e: Exception) {
-                _errorState.postValue(e.message)
+        private fun loadCountries() {
+            viewModelScope.launch {
+                try {
+                } catch (e: Exception) {
+                    _errorState.postValue(e.message)
+                }
             }
         }
-    }
 
-    fun loadRegions(countryId: String) {
-        viewModelScope.launch {
-            try {
-            } catch (e: Exception) {
-                _errorState.postValue(e.message)
+        fun loadRegions(countryId: String) {
+            viewModelScope.launch {
+                try {
+                } catch (e: Exception) {
+                    _errorState.postValue(e.message)
+                }
             }
         }
-    }
 
-    // Метод для обработки выбора страны
-    fun onCountrySelected(country: String) {
-        loadRegions(country)
-    }
+        // Метод для обработки выбора страны
+        fun onCountrySelected(country: String) {
+            loadRegions(country)
+        }*/
 }
